1da63d98b5794a424924215b81fc167640b38653
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-13 16:14:29 +0100

    refactoring

--- twosums.js
+++ twosums.js
@@ -6,10 +6,11 @@
  var twoSum = function(nums, target) {
   for (let i = 0 ; i < nums.length ; i++ ) {
       if (target - nums[i+1] === nums[i]) {
-          return [nums.indexOf(nums[i]),nums.indexOf(nums[i+1])];
+          return [i,i+1];
       }
   }
 };
 
 console.log(twoSum([2,7,11,15],9))
 console.log(twoSum([3,2,4],6))
+console.log(twoSum([3,3],6))
