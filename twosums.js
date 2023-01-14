ea3b3cd08503c785715ffe5d7e1a80ccbf9f7e05
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-14 12:52:56 +0100

    updated solution

--- twosums.js
+++ twosums.js
@@ -8,9 +8,11 @@
       if (target - nums[i+1] === nums[i]) {
           return [i,i+1];
       }
+      if (target - nums[i+2] === nums[i]) {
+        return [i,i+2];
   }
-};
-
+  };
+ }
 console.log(twoSum([2,7,11,15],9))
-console.log(twoSum([3,2,4],6))
+console.log(twoSum([3,2,3],6))
 console.log(twoSum([3,3],6))
