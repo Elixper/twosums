1511883ddfedc95f10be7c0c75eeb27b2438977e
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-14 13:31:10 +0100

    first solution

--- twosums.js
+++ twosums.js
@@ -3,16 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-  for (let i = 0 ; i < nums.length ; i++ ) {
-      if (target - nums[i+1] === nums[i]) {
-          return [i,i+1];
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target - nums[i] === nums[j]) {
+        return [i, j];
       }
-      if (target - nums[i+2] === nums[i]) {
-        return [i,i+2];
+    }
   }
-  };
- }
-console.log(twoSum([2,7,11,15],9))
-console.log(twoSum([3,2,3],6))
-console.log(twoSum([3,3],6))
+};
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 3], 6));
+console.log(twoSum([3, 3], 6));
